6ed10de8b9733c72fc3e3c54407b521d967e981f
KALYAN <allam.kalyana@outlook.com>
2023-11-26 02:24:20 +0530

    Removed hardcoding

---
+++ MauiAppMCQs/Models/InQuestion .cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppMCQs.Models
+{
+
+    public class InQuestion
+
+
+    {
+
+        public int No { get; set; }
+
+        public string QuestionTitle { get; set; }
+
+        public string Opt1 { get; set; }
+
+        public string Opt2 { get; set; }
+
+        public string Opt3 { get; set; }
+
+        public string Opt4 { get; set; }
+
+        public string Answer { get; set; }
+    }
+}
--- MauiAppMCQs/Pages/QuizCardBase.cs
+++ MauiAppMCQs/Pages/QuizCardBase.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MauiAppMCQs.Models;
-
+using Newtonsoft.Json;
 namespace MauiAppMCQs.Pages
 {
     public class QuizCardBase : ComponentBase
@@ -12,7 +12,7 @@ namespace MauiAppMCQs.Pages
         protected string[] failedQuestions= new string[100];
         protected override Task OnInitializedAsync()
         {
-            LoadQuestions();
+            LoadQuestionsAsync();
 
             return base.OnInitializedAsync();
         }
@@ -20,195 +20,81 @@ namespace MauiAppMCQs.Pages
 
         protected void OptionSelected(string option)
         {
-            if (option == Questions[questionIndex].Answer)
+            if (option.Trim() == Questions[questionIndex].Answer.Trim())
             {
                 score++;
             }
             else
             {
-                failedQuestions[failedIndex] = Questions[questionIndex].QuestionTitle;
+                failedQuestions[failedIndex] = Questions[questionIndex].QuestionTitle + " " + Questions[questionIndex].Answer;
                 failedIndex++;
 
 
             }
             questionIndex++;
         }
-
+        protected void TakeQuiz()
+        {
+            score = 0;
+            questionIndex = 0;
+        }
         protected void RestartQuiz()
         {
             score = 0;
             questionIndex = 0;
         }
-
-        private void LoadQuestions()
+        async Task<List<InQuestion>> GetApiData()
         {
-            Question q1 = new Question
+            string apiUrl = "https://sheet2api.com/v1/wlS0h0USxm9p/quiz";
+            List<InQuestion> Questions = new List<InQuestion>();
+            using (HttpClient client = new HttpClient())
             {
-                QuestionTitle = "What is the capital of France?",
-                Options = new List<string>() { "Nantes", "Lyon", "Paris", "Bern" },
-                Answer = "Paris"
-            };
+                try
+                {
+                    // Make a GET request to the API
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
 
-            Question q2 = new Question
-            {
-                QuestionTitle = "What is the capital of England?",
-                Options = new List<string>() { "London", "Manchester", "Glasgow", "Edinburgh" },
-                Answer = "London"
-            };
-
-            Question q3 = new Question
-            {
-                QuestionTitle = "What is the capital of Japan?",
-                Options = new List<string>() { "Tokyo", "Seoul", "Kyoto", "Jakarta" },
-                Answer = "Tokyo"
-            };
-
-            Question q4 = new Question
-            {
-                QuestionTitle = "What is the capital of Bangladesh?",
-                Options = new List<string>() { "Islamabad", "Delhi", "Dakka", "Kolkata" },
-                Answer = "Dakka"
-            };
+                    // Check if the request was successful (status code 200)
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Read the content of the response
+                        string jsonString = await response.Content.ReadAsStringAsync();
 
 
-            Question q5 = new Question
-            {
-                QuestionTitle = "What is the capital of Egypt?",
-                Options = new List<string>() { "Riyadh", "Cairo", "Tel Aviv", "Dubai" },
-                Answer = "Cairo"
-            };
+                        // Deserialize the JSON string into an object
+                        Questions = JsonConvert.DeserializeObject<List<InQuestion>>(jsonString);
 
-            Question q6 = new Question
-            {
-                QuestionTitle = "What is the capital of Mongolia?",
-                Options = new List<string>() { "Nairobi", "Shanghai", "Hubei", "Ulan Bator" },
-                Answer = "Ulan Bator"
-            };
-            Question q7 = new Question
-            {
-                QuestionTitle = "What is the capital of Iran?",
-                Options = new List<string>() { "Riyadh", "Tehran", "Bagdhad", "Kabul" },
-                Answer = "Tehran"
-            };
-            Question q8 = new Question
-            {
-                QuestionTitle = "What is the capital of Portugal?",
-                Options = new List<string>() { "Lisbon", "Madrid", "Barcelona", "Milan" },
-                Answer = "Lisbon"
-            };
-            Question q9 = new Question
-            {
-                QuestionTitle = "What is the capital of USA?",
-                Options = new List<string>() { "London", "Washington DC", "Dublin", "Toronto" },
-                Answer = "Washington DC"
-            };
-            Question q10 = new Question
-            {
-                QuestionTitle = "What is the capital of Argentina?",
-                Options = new List<string>() { "Mexico City", "Bogota", "Buenos Aires", "Brasillia" },
-                Answer = "Buenos Aires"
-            };
-            Question q11= new Question
-            {
-                QuestionTitle = "What is the capital of Ukraine?",
-                Options = new List<string>() { "Kyiv", "Minsk", "Budapest", "Bucharest" },
-                Answer = "Kyiv"
-            };
-            Question q12 = new Question
-            {
-                QuestionTitle = "What is the capital of Turkey?",
-                Options = new List<string>() { "Damascus", "Athens", "Riyadh", "Ankara" },
-                Answer = "Ankara"
-            };
-            Question q13 = new Question
-            {
-                QuestionTitle = "What is the capital of Kenya?",
-                Options = new List<string>() { "Lusaka", "Kampala", "Nairobi", "Addis Ababa" },
-                Answer = "Nairobi"
-            };
-            Question q14 = new Question
-            {
-                QuestionTitle = "What is the capital of Zaire?",
-                Options = new List<string>() { "Lusaka", "Maputo", "Nairobi", "Kinshasa" },
-                Answer = "Kinshasa"
-            };
-            Question q15= new Question
-            {
-                QuestionTitle = "What is the capital of Ghana?",
-                Options = new List<string>() { "Accra", "Maputo", "Nairobi", "Kinshasa" },
-                Answer = "Accra"
-            };
-
-            Question q16 = new Question
-            {
-                QuestionTitle = "What is the capital of New Zealand?",
-                Options = new List<string>() { "Melbourne", "Auckland", "Wellington", "Sydney" },
-                Answer = "Wellington"
-            };
-            Question q17 = new Question
-            {
-                QuestionTitle = "What is the capital of Lithuania?",
-                Options = new List<string>() { "Minsk", "Leningrad", "Vilnuis", "Moscow" },
-                Answer = "Vilnuis"
-            };
-            Question q18 = new Question
-            {
-                QuestionTitle = "What is the capital of Canada?",
-                Options = new List<string>() { "Toronto", "London", "Ottawa", "Vancouver" },
-                Answer = "Ottawa"
-            };
-            Question q19 = new Question
-            {
-                QuestionTitle = "What is the capital of India?",
-                Options = new List<string>() { "New Delhi", "Ahmedabad", "Kolkata", "Mumbai" },
-                Answer = "New Delhi"
-            };
-
-            Question q20 = new Question
-            {
-                QuestionTitle = "What is the capital of Afghanistan?",
-                Options = new List<string>() { "Tashkent", "Bagdhad", "Kabul", "Sharjah" },
-                Answer = "Kabul"
-            };
-
-
-            Question q21 = new Question
-            {
-                QuestionTitle = "What is the capital of Thailand?",
-                Options = new List<string>() { "Jakarta", "Bangkok", "Hanoi", "Manila" },
-                Answer = "Bangkok"
-            };
-
-
-            Question q22 = new Question
-            {
-                QuestionTitle = "What is the capital of Indonesia?",
-                Options = new List<string>() { "Jakarta", "Bangkok", "Seoul", "Kualalampur" },
-                Answer = "Jakarta"
-            };
-
-            Question q23 = new Question
-            {
-                QuestionTitle = "What is the capital of Peru?",
-                Options = new List<string>() { "Santiago", "Lima", "Buenos Aries", "Quito" },
-                Answer = "Lima"
-            };
-
-            Question q24 = new Question
-            {
-                QuestionTitle = "What is the capital of Costa Rica?",
-                Options = new List<string>() { "Managua", "San Salvador", "San Jose", "Quito" },
-                Answer = "San Jose"
-            };
-
-            Question q25 = new Question
-            {
-                QuestionTitle = "What is the capital of Fiji?",
-                Options = new List<string>() { "Port Vila", "Suva", "Papeete", "Port Vila" },
-                Answer = "Suva"
-            };
-
-            Questions.AddRange(new List<Question> { q1, q2, q3, q4, q5 ,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17,q18,q19,q20,q21,q22,q23,q24,q25});
+                        
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+            }
+            return Questions;
         }
+
+        private async Task LoadQuestionsAsync()
+        {
+
+
+
+
+            List<InQuestion> res = await GetApiData();
+
+            Questions.AddRange(res.Select(r => new Question
+            {
+                QuestionTitle = r.QuestionTitle,
+                Options = new List<string>() { r.Opt1, r.Opt2, r.Opt3, r.Opt4 },
+                Answer = r.Answer
+            }));
+
+        }
+
     }
 }
