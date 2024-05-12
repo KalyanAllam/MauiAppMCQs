1af9411cbeaaefbe6a12cdaa0e7480908aea15aa
KALYAN <allam.kalyana@outlook.com>
2024-05-12 13:45:07 +0530

    Added coded

---
+++ MauiAppMCQs/Models/QuestionI.cs
@@ -0,0 +1,46 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppMCQs.Models
+{
+    public class QuestionI
+
+
+    {
+       
+
+
+        public int SNo { get; set; }
+        public int No { get; set; }
+
+        public string Topic { get; set; }
+
+        public string QuestionTitle { get; set; }
+
+        public string Opt1 { get; set; }
+
+        public string Opt2 { get; set; }
+
+        public string Opt3 { get; set; }
+
+        public string Opt4 { get; set; }
+
+        public string Answer { get; set; }
+
+        public int Time { get; set; }
+
+
+        public int Correct { get; set; }
+
+        public string Solution { get; set; }
+        public byte[] Imagedata { get; set; }
+        internal IEnumerable<Question> Select(Func<object, Question> value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
