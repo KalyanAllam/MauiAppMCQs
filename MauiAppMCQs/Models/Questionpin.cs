a7e1ba16916f9066c3ab6b3efc281cc046a52526
KALYAN <allam.kalyana@outlook.com>
2024-08-05 01:05:53 +0530

    pins

---
+++ MauiAppMCQs/Models/Questionpin.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppMCQs.Models
+{
+    public class Questionpin
+    {
+        [PrimaryKey, Indexed]
+
+
+        public int ID { get; set; }
+        public int pin { get; set; }
+
+        
+
+        internal IEnumerable<Question> Select(Func<object, Question> value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
